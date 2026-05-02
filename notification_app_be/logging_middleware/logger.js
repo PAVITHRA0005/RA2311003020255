18f1eeb114f43e6e8ee0109e563badb418438239
Pavithra S <rspavithra05@gmail.com>
2026-05-02 17:06:28 +0530

    add3

--- notification_app_be/logging_middleware/logger.js
+++
@@ -1,9 +0,0 @@
-const logger = (req, res, next) => {
-    const time = new Date().toISOString();
-
-    console.log(`${req.method} ${req.url} - ${time}`);
-
-    next(); 
-};
-
-module.exports = logger;
